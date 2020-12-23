df9254c46dd91b4b3cb29fdffab3389331d479d9
Kenny Whyte <kawhyte@gmail.com>
2020-12-23 08:36:46 -0800

    bg image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,10 +20,10 @@ const Header = () => {
   `)
 
   return (
-    <div className="container pt-6 pb-12 md:pt-12">
+    <div className="container pt-2 pb-4 md:pt-8">
       <div className="flex justify-between items-center">
         <Link to="/">
-          <img alt="Logo" className="w-24 md:w-32" src="logo.svg" />
+          <img alt="Logo" className="w-40 md:w-48" src="logo2.svg" />
         </Link>
 
         <button
@@ -39,7 +39,7 @@ const Header = () => {
             <Link
               key={`menu_desktop_link${key}`}
               className="ml-6 sm:ml-8 text-sm sm:text-base font-medium px-px border-b-2 pb-2 border-transparent text-gray-700 hover:text-gray-800 hover:border-gray-200 transition duration-150 ease-in-out"
-              activeClassName="border-blue-600 text-gray-900 hover:border-blue-600"
+              activeClassName="border-yellow-500 text-gray-900 hover:border-yellow-500"
               to={link.to}
             >
               {link.name}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,57 @@
 import React from "react"
+import { Link } from "gatsby";
+
+import BG from "../images/mainCake.webp";
 
 const Hero = () => (
-  <div className="container py-12 lg:pb-16">
-    <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
-      Hello, I'm John{" "}
-      <span role="img" aria-label="waving hand">
-        👋
-      </span>
-      <br />
-      <span className="text-blue-600">
-        Welcome to my photography portfolio.
-      </span>
-    </h2>
-  </div>
+  <div>
+  <main>
+    <div
+      className='relative pt-16 pb-32 flex content-center items-center justify-center'
+      style={{
+        minHeight: "75vh",
+      }}>
+      <div
+        className='absolute top-0 w-full h-full bg-center bg-cover'
+        style={{
+          backgroundImage: `url('${BG}')`,
+        }}>
+        <span
+          id='blackOverlay'
+          className='w-full h-full absolute opacity-25 bg-black'></span>
+      </div>
+      <div className='container relative mx-auto'>
+        <div className='items-center flex flex-wrap sm:px-24 '>
+          <div className='w-full lg:w-9/12 px-4 ml-auto mr-auto text-center'>
+            <div className='pr-12'>
+              <h2 className='text-4xl tracking-tight leading-10 font-extrabold text-yellow-500 sm:text-4xl sm:leading-none md:text-6xl'>
+               Fancyflower Cakes
+              </h2>
+
+              <p className='mt-4 mb-4 text-base md:text-2xl text-gray-300 '>
+                Cakes For All Your  Special Occasions. 
+              </p>
+              <Link to='/prints'>
+                <button
+                  className={
+                    "bg-yellow-500 text-black  active:bg-gray-100" +
+                    " text-xs sm:text-sm font-bold uppercase px-4 py-4 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                  }
+                  type='button'
+                  style={{ transition: "all .15s ease" }}>
+                  <i className='fas fa-arrow-alt-circle-down'></i>Get a Quote
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+ 
+    </div>
+
+
+  </main>
+</div>
 )
 
 export default Hero
